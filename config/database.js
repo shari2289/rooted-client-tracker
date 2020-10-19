1b0f01e9a80dce9467c026c74fd0e82e4e17362c
Shari Reyes <shari2289@yahoo.com>
2020-10-19 04:39:37 -0500

    attempt to fix auth

--- config/database.js
+++ config/database.js
@@ -9,6 +9,6 @@ mongoose.connect(process.env.DATABASE_URL, {
 const db = mongoose.connection;
 
 // database connection event
-db.on('connected', function () {
+db.once('connected', () => {
   console.log(`Mongoose connected to: ${db.host}:${db.port}`);
 });
